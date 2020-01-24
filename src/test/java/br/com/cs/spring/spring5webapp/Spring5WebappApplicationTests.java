54b8ed6b1c0ab7e392f11b4477a1cc67d0ff3c1c
cspower2016 <ecarlos.a.silva@gmail.com>
2020-01-24 08:56:04 -0300

    Add base project code plus JPA Hibernate initial stuff

---
+++ src/test/java/br/com/cs/spring/spring5webapp/Spring5WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cs.spring.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
